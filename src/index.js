9614ba2006af652e59eb8e42b0fe51ee8b085253
sinsangkug <ssk3924@naver.com>
2024-05-14 05:46:18 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//index.html파일 내부에 작성된 root id속성 값을 가진 <div> 엘리먼트를 선택해 root변수에 저장
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//<div> 엘리먼트 영역 내부에  App.js에 작성된     <div className="App">...</div>  App컴포넌트의 디자인을 넣어 보여주게 설정
 root.render(
   <React.StrictMode>
     <App />
